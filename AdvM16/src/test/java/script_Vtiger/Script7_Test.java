5c859293220d49e80b7858147fe35ff89d52be19
Chandana N <Chandana N@TYSS-Chandana>
2024-07-22 08:55:22 +0530

    i have added script7 inside script_Vtiger package

---
+++ AdvM16/src/test/java/script_Vtiger/Script7_Test.java
@@ -0,0 +1,12 @@
+package script_Vtiger;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Script7_Test {
+
+	@Test
+	public void script7() {
+		Reporter.log("script7",true);
+	}
+}
